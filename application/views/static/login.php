5dedf68875e2272238999c2baf95bcf0a582d946
Rizal0063 <48111980+Rizal0063@users.noreply.github.com>
2019-12-10 22:11:34 +0700

    Update login.php

--- application/views/static/login.php
+++ application/views/static/login.php
@@ -1,30 +1,58 @@
+<!DOCTYPE html>
 <html>
-
 <head>
-
+	<title>Sistem Informasi Kerja Praktik</title>
+	<link rel="stylesheet" href="<?=base_url('vendors/font-awesome/css/font-awesome.min.css')?>">
+	<link rel="stylesheet" href="<?=base_url('vendors/coreui/css/style.min.css')?>">
+	<link rel="stylesheet" href="<?=base_url('vendors/coreui/icons/css/coreui-icons.min.css')?>">
+	<script src="<?=base_url('vendors/jquery/jquery.min.js')?>"></script>
+	<script src="<?=base_url('vendors/bootstrap/bootstrap.min.js')?>"></script>
+	<script src="<?=base_url('vendors/coreui/js/coreui.min.js')?>"></script>
+	<script src="<?=base_url('assets/style.css')?>"></script>
+	<link rel="icon" href="<?=base_url('assets/logo.png')?>">
 </head>
-
-<body>
-    <form method="POST" action="">
-        <table align="center">
-            <tr>
-                <td>
-                    <font2>Username : </font2>
-                </td>
-                <td><input type="text" name="username" placeholder="Masukkan Username" class="textbox" />
-            </tr>
-            <tr>
-                <td>
-                    <font2>Password : </font2>
-                </td>
-                <td><input type="password" name="password" placeholder="Masukkan Password" class="textbox" />
-            </tr>
-            <tr>
-                <td colspan="2"><input type="submit" name="submit" value="Login" class="button" />&nbsp;<input
-                        type="reset" value="Reset" class="button"></td>
-            </tr>
-        </table>
-    </form>
-</body>
-
+  <body class="app flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card-group">
+            <div class="card p-4">
+              <div class="card-body">
+                <h1>Login</h1>
+                <p class="text-muted">Sign In to your account</p>
+                <div class="input-group mb-3">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="icon-user"></i>
+                    </span>
+                  </div>
+                  <input class="form-control" type="text" placeholder="Username">
+                </div>
+                <div class="input-group mb-4">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="icon-lock"></i>
+                    </span>
+                  </div>
+                  <input class="form-control" type="password" placeholder="Password">
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <button class="btn btn-primary px-4" type="button">Login</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CoreUI and necessary plugins-->
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/pace-progress/pace.min.js"></script>
+    <script src="node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+    <script src="node_modules/@coreui/coreui/dist/js/coreui.min.js"></script>
+  </body>
 </html>
