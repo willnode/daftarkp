c8ac3518150db16963a319d45b71c6d296acbe1e
Rizal0063 <48111980+Rizal0063@users.noreply.github.com>
2019-12-12 22:02:56 +0700

    edit

--- application/views/admin/edit/dosen.php
+++ application/views/admin/edit/dosen.php
@@ -1,36 +1,7 @@
 <form class="form-horizontal" action="<?=base_url("admin/dosen/update/$data->id_dosen")?>" method="post" enctype="multipart/form-data">
-<div class="form-group row">
-<label class="col-md-3 col-form-label" for="nip_dosen">NIP</label>
-<div class="col-md-9">
-    <input class="form-control" id="nip_dosen" type="text" name="nip_dosen" value="<?= $data->nip_dosen?>">
-</div>
-</div>
-
-<div class="form-group row">
-<label class="col-md-3 col-form-label" for="nama_dosen">Nama</label>
-<div class="col-md-9">
-    <input class="form-control" id="nama_dosen" type="text" name="nama_dosen" value="<?= $data->nama_dosen?>">
-</div>
-</div>
-
-<div class="form-group row">
-        <label class="col-md-3 col-form-label" for="username">Username</label>
-        <div class="col-md-9">
-            <input class="form-control" id="username" type="text" name="username" value="<?= $data->username?>">
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <label class="col-md-3 col-form-label" for="password">Password</label>
-        <div class="col-md-9">
-            <input class="form-control" id="password" type="password" name="password">
-        </div>
-    </div>
-
-<div class="form-group row">
-<label class="col-md-3 col-form-label"></label>
-<div class="col-md-9">
-    <input type="submit" class="form-control btn btn-primary" value="Submit">
-</div>
-</div>
+<?php form_input(['name'=>'nip_dosen', 'label'=>'NIP', 'value'=>$data->nip_dosen]) ?>
+<?php form_input(['name'=>'nama_dosen', 'label'=>'Nama', 'value'=>$data->nama_dosen]) ?>
+<?php form_input(['name'=>'username', 'label'=>'Username', 'value'=>$data->username]) ?>
+<?php form_input(['name'=>'password', 'label'=>'Password', 'value'=>$data->password]) ?>
+<?php form_input(['type'=>'submit', 'class'=>'form-control btn btn-primary', 'value'=>"Submit"]) ?>
 </form>
--- application/views/admin/edit/mahasiswa.php
+++ application/views/admin/edit/mahasiswa.php
@@ -1,52 +1,12 @@
 <form class="form-horizontal" action="<?=base_url("admin/mahasiswa/update/$data->id_mahasiswa")?>" method="post"
     enctype="multipart/form-data" name="form">
-    <div class="form-group row">
-        <label class="col-md-3 col-form-label" for="nim_mahasiswa">NIM</label>
-        <div class="col-md-9">
-            <input class="form-control" id="nim_mahasiswa" type="text" name="nim_mahasiswa" value="<?= $data->nim_mahasiswa?>"
-                oninput="window.form.username.value = window.form.nim_mahasiswa.value+'@student.trunojoyo.ac.id'">
-        </div>
-    </div>
 
-    <div class="form-group row">
-        <label class="col-md-3 col-form-label" for="nama_mahasiswa">Nama</label>
-        <div class="col-md-9">
-            <input class="form-control" id="nama_mahasiswa" type="text" name="nama_mahasiswa" value="<?= $data->nama_mahasiswa?>">
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <label class="col-md-3 col-form-label" for="prodi_mahasiswa">Prodi</label>
-        <div class="col-md-9">
-            <select class="form-control form-control" id="prodi_mahasiswa" name="prodi_mahasiswa">
-                <?php foreach ($prodi_mahasiswa as $p) : ?>
-                <option value="<?= $p->id_prodi_mahasiswa ?>" <?=$data->prodi_mahasiswa==$p->id_prodi_mahasiswa ? 'selected' : ''?>>
-                    <?= $p->nama_prodi_mahasiswa?></option>
-                <?php endforeach ?>
-            </select>
-        </div>
-    </div>
-
-
-    <div class="form-group row">
-        <label class="col-md-3 col-form-label" for="username">Username</label>
-        <div class="col-md-9">
-            <input class="form-control" id="username" type="text" name="username" readonly value="<?= $data->username?>">
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <label class="col-md-3 col-form-label" for="password">Password</label>
-        <div class="col-md-9">
-            <input class="form-control" id="password" type="password" name="password">
-        </div>
-    </div>
-
-
-    <div class="form-group row">
-        <label class="col-md-3 col-form-label"></label>
-        <div class="col-md-9">
-            <input type="submit" class="form-control btn btn-primary" value="Submit">
-        </div>
-    </div>
+    <?php form_input(['name'=>'nim_mahasiswa', 'label'=>'NIM', 'value'=>$data->nim_mahasiswa,
+             'oninput'=>"window.form.username.value = window.form.nim_mahasiswa.value+'@student.trunojoyo.ac.id'"]) ?>
+    <?php form_input(['name'=>'nama_mahasiswa', 'label'=>'Nama', 'value'=>$data->nama_mahasiswa]) ?>
+    <?php form_option(['name'=>'prodi_mahasiswa', 'label'=>'Prodi', 'value'=>$data->id_prodi,
+                        'options'=>$prodi, 'option_key'=>'id_prodi', 'option_value'=>'nama_prodi']) ?>
+    <?php form_input(['name'=>'username', 'label'=>'Username', 'value'=>$data->username]) ?>
+    <?php form_input(['name'=>'password', 'label'=>'Password', 'value'=>$data->password, 'type'=>'password']) ?>
+    <?php form_input(['type'=>'submit', 'class'=>'form-control btn btn-primary', 'value'=>"Submit"]) ?>
 </form>
