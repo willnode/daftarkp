6e91b6de25d6764c7c1cf279056877b4efd6db3d
Rizal0063 <48111980+Rizal0063@users.noreply.github.com>
2019-12-12 21:00:18 +0700

    Update surat.php

--- application/views/dosen/surat.php
+++ application/views/dosen/surat.php
@@ -7,6 +7,7 @@
                           <th>Nama Perusahaan</th>
                           <th>Alamat Perusahaan</th>
                           <th>Jangka Waktu</th>
+                          <th>Verifikasi Koordinator</th>
                           <th>Action 
 
                           <a href="<?= base_url("admin/surat/create")?>" class="btn btn-primary"><i class="fa fa-plus"></i></a>
@@ -22,6 +23,7 @@
                           <td><?= $surat->nama_perusahaan?></td>
                           <td><?= $surat->alamat_perusahaan?></td>
                           <td><?= $surat->jangka_waktu?></td>
+                          <td><?= $surat->verifikasi_koordinator?></td>
                           <td><a href="<?= base_url("admin/surat/edit/$surat->id_surat") ?>" class="btn btn-warning"><i class="fa fa-edit"></i></a> 
                           <a href="<?= base_url("admin/surat/delete/$surat->id_surat")?>" class="btn btn-danger"><i class="fa fa-trash"></i></a>
                         </td>
