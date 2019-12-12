6862f9bcf8f3ae431737e89fea7de49f3be587dc
Afaisw <feizholreddevils@gmail.com>
2019-12-13 00:24:09 +0700

    Upload widget

--- application/helpers/form_kit_helper.php
+++ application/helpers/form_kit_helper.php
@@ -42,3 +42,35 @@ function form_option($attr=[]) {
     </div>
     <?php
 }
+
+function form_file($attr=[]) {
+    $name = isset($attr['name']) ? $attr['name'] : '';
+    $label = isset($attr['label']) ? $attr['label'] : '';
+    $file = './uploads/'.$attr['folder'].'/'.$attr['value'];
+    $attr['class'] = isset($attr['class']) ? $attr['class'] : 'form-control';
+    ?>
+    <div class="form-group row">
+        <label class="col-md-3 col-form-label" for="<?=$name?>"><?=$label?></label>
+        <div class="col-md-9">
+            <?php if (!isset($attr['readonly'])) : ?>
+            <input id="<?=$name?>" type="file" <?= implode(' ', array_map(
+                            function ($k, $v) { return $k .'="'. htmlspecialchars($v) .'"'; },
+                            array_keys($attr), $attr
+                        ))  ?>>
+            <?php endif ?>
+            <?php if (file_exists($file)) : ?>
+            <a href="<?=base_url($file)?>" class="btn btn-info"><i class="fa fa-download"></i> Unduh</a>
+            <?php endif ?>
+        </div>
+    </div>
+
+    <?php
+}
+
+function form_file_upload($name, $folder)
+{
+    $this->upload->initialize([
+        'upload_path' => './uploads/'.$folder.'/',
+    ]);
+    return $this->upload->do_upload($name);
+}
